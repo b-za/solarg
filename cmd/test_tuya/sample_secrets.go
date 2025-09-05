7b96def2ef57e5b5dfdfbeeccc11ec20e31e5556
Bernard Hamann <4625028+b-za@users.noreply.github.com>
2025-09-05 16:25:12 +0200

    Create sample_secrets.go

---
+++ cmd/test_tuya/sample_secrets.go
@@ -0,0 +1,6 @@
+package main
+
+// Uncomment and replace with your own values
+// const TuyaClientId = "YOUR_CLIENT_ID"
+// const TuyaClientSecret = "YOUR_CLIENT_SECRET"
+// const TuyaDeviceID = "YOUR_DEVICE_ID"
