9e095e44db6136282e0ca0171bc86ed045ec7a1d
Bernard Hamann <4625028+b-za@users.noreply.github.com>
2025-09-08 08:02:54 +0200

    Update main.go

--- cmd/solarg/main.go
+++ cmd/solarg/main.go
@@ -1,20 +1,28 @@
 package main
 
 import (
-	"flag"
 	"fmt"
 	"log"
-	"os"
 	"time"
 
 	"github.com/b-za/solarg/internal/fox"
+	"github.com/b-za/solarg/internal/tuya"
 )
+
+const foxApiKey = FOX_API_KEY
+const foxInverterSerialNumber = FOX_INVERTER_SERIAL_NUMBER
+
+const TuyaClientId = TUYA_CLIENT_ID
+const TuyaClientSecret = TUYA_CLIENT_SECRET
+const TuyaDeviceID = TUYA_DEVICE_ID
 
 // --- Configuration ---
 // Set the start and end times for the active window in "HH:MM" format.
 const (
-	startTimeStr = "09:00"
+	startTimeStr = "07:00"
 	endTimeStr   = "17:00"
+	batteryMin   = 60.00
+	BatteryMax   = 80.00
 	// Set the location to ensure the time comparison is accurate.
 	// SAST (South Africa Standard Time) corresponds to "Africa/Johannesburg".
 	locationName = "Africa/Johannesburg"
@@ -31,7 +39,11 @@ func main() {
 	log.Printf("Active window is between %s and %s (%s).", startTimeStr, endTimeStr, locationName)
 
 	// Create a ticker that fires every 5 minutes.
-	ticker := time.NewTicker(5 * time.Minute)
+	//ticker := time.NewTicker(5 * time.Minute)
+
+	// Ticker every 5 seconds
+	ticker := time.NewTicker(30 * time.Second)
+
 	// Ensure the ticker is stopped when the function exits to clean up resources.
 	defer ticker.Stop()
 
@@ -72,34 +84,55 @@ func checkTime(location *time.Location) {
 	// Check if the current time is after the start and before the end.
 	if now.After(activeStartTime) && now.Before(activeEndTime) {
 		log.Printf("[%s] The current time is WITHIN the active window.", now.Format("15:04:05"))
+		activeWindowLoop()
 	} else {
 		log.Printf("[%s] The current time is NOT within the active window.", now.Format("15:04:05"))
 	}
 }
 
-func getBatteryStatus() {
+func activeWindowLoop() {
 
-	foxApiKey := FOX_API_KEY
-	foxInverterSerialNumber := FOX_INVERTER_SERIAL_NUMBER
+	var batteryPercentage float64
+	// var residualEnergy float64
+	var geyserOnStatus bool
 
 	// This is the formula for estimating the battery percentage
 	// Change the divide by as needed to improve the estimate
 	//"[(residualEnergy / divideBy) * 100]"
-	divideBy := 6.6
+	var divideBy = 6.6
 
-	if foxApiKey == "" || foxInverterSerialNumber == "" {
-		fmt.Println("❌ Error: No foxApiKey or foxInverterSerialNumber")
-		flag.Usage()
-		os.Exit(1)
+	_, batteryPercentage = fox.GetBatteryStatus(foxApiKey, foxInverterSerialNumber, divideBy)
+
+	fmt.Println("Battery % is:", batteryPercentage)
+
+	if batteryPercentage > BatteryMax {
+		if geyserOnStatus == false {
+			fmt.Println("Battery % is > 80, and the geyser is off. Turn on the geyser")
+			response, err := tuya.SetSwitchState(TuyaDeviceID, TuyaClientId, TuyaClientSecret, false)
+			if err != nil {
+				log.Fatalf("Failed to set switch state: %v", err)
+			}
+			fmt.Println("API Response:")
+			fmt.Println(response)
+
+		} else {
+			fmt.Println("Battery % is > 80, and the geyser is on. Leave the geyser alone.")
+		}
+	} else if batteryPercentage < batteryMin {
+		if geyserOnStatus == true {
+			fmt.Println("Battery % is < 60, and the geyser is on. Turn off the geyser")
+			response, err := tuya.SetSwitchState(TuyaDeviceID, TuyaClientId, TuyaClientSecret, false)
+			if err != nil {
+				log.Fatalf("Failed to set switch state: %v", err)
+			}
+			fmt.Println("API Response:")
+			fmt.Println(response)
+		} else {
+			fmt.Println("Battery % is < 60, and the geyser is off. Leave the geyser alone.")
+		}
+
+	} else {
+		fmt.Println("Battery % is between 60 and 80. Leave the geyser alone.")
 	}
 
-	residualEnergy, batteryStatus := fox.GetBatteryStatus(foxApiKey, foxInverterSerialNumber, divideBy)
-
-	fmt.Println("residualEnergy")
-	fmt.Println(residualEnergy)
-	fmt.Println("divideBy")
-	fmt.Println(divideBy)
-	fmt.Println("batteryStatus")
-	fmt.Println("[(residualEnergy / divideBy) * 100]")
-	fmt.Println(batteryStatus)
 }
